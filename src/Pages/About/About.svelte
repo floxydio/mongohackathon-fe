eb91f431f335499318c9af1ef1d4c05d250df0a5
floxydio <diooktar@gmail.com>
2022-11-21 20:09:19 +0700

    update

--- src/Pages/About/About.svelte
+++ src/Pages/About/About.svelte
@@ -4,8 +4,28 @@
 
 
 <div>
-    <p>About</p>
+
+    <div class="about__class">
+        <i class="fab fa-linkedin"></i> Dio Okta Rovelino
+        <i class="fab fa-github"></i> floxydio
+    </div>
+
 </div>
 
 
-<style></style>
+<style>
+    .about__class {
+        text-align: center;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;    
+    }
+    .about__class i {
+        font-size: 2rem;
+        color: #0077b5;
+        padding: 10px;
+    }
+
+
+
+</style>
